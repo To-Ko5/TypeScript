ce2699b8891bda3def3e2e5b553fdf59cb4664a7
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-02-21 08:22:48 +0900

    file change

---
+++ src/first.ts
@@ -0,0 +1,25 @@
+const hasValue: boolean = true
+
+// object
+const person: { name: string; age: number } = {
+  name: 'demo',
+  age: 20
+}
+
+// array
+const fruits: string[] = ['apple', 'banana', 'strawberry']
+const fruits2: (string | number)[] = ['apple', 'banana', 'strawberry']
+
+// tuple
+const book: [string, number, boolean] = ['story', 2000, true]
+
+// enum
+enum Size {
+  short = 1,
+  medium = 2,
+  big = 3
+}
+const drink = {
+  hot: true,
+  size: Size.big
+}
