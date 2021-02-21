275792fbc0ec57769cb8f340336bca597883dea1
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-02-22 08:11:17 +0900

    public private

--- src/class.ts
+++ src/class.ts
@@ -1,13 +1,20 @@
 class Person {
-  name: string
-  constructor(name: string) {
+  public name: string // 初期値はpublic
+  private age: number // private class内のみで参照
+
+  constructor(name: string, age: number) {
     this.name = name
+    this.age = age
+  }
+
+  incrementAge() {
+    this.age += 1
   }
 
   greeting() {
-    console.log(`Hello ${this.name}`)
+    console.log(`Hello ${this.name}  ${this.age} `)
   }
 }
 
-const demo: Person = new Person('tom')
+const demo: Person = new Person('tom', 50)
 console.log(demo.greeting())
