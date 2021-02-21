acaf7a2fd1bc73c85d12e345b1385ac4b8432c97
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-02-22 08:18:14 +0900

    readonly

--- src/class.ts
+++ src/class.ts
@@ -1,10 +1,12 @@
 class Person {
   public name: string // 初期値はpublic
   private age: number // private class内のみで参照
+  public readonly comment: string // readonly 書き換えせず、参照のみ
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, comment: string) {
     this.name = name
     this.age = age
+    this.comment = comment
   }
 
   incrementAge() {
@@ -12,9 +14,10 @@ class Person {
   }
 
   greeting() {
-    console.log(`Hello ${this.name}  ${this.age} `)
+    console.log(` ${this.name} ${this.age} ${this.comment} `)
   }
 }
 
-const demo: Person = new Person('tom', 50)
+const demo: Person = new Person('tom', 50, 'hello')
 console.log(demo.greeting())
+console.log(demo.comment)
