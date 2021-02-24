81b6e7dbc1b88750b6e4a50f194dbbd79e03ab44
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-02-24 21:36:36 +0900

    count 機能を追加

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,76 @@
-class Score {}
+class Score {
+  private static instance: Score
+  get totalScore() {
+    const count = Counts.getInstance()
+    return count.activeElementsScore.reduce((total, score) => total + score + 0)
+  }
+
+  render() {
+    document.querySelector('.score__number')!.textContent = String(
+      this.totalScore
+    )
+  }
+
+  private constructor() {}
+
+  static getInstance() {
+    if (!Score.instance) {
+      Score.instance = new Score()
+    }
+    return Score.instance
+  }
+}
 
 class Count {
   constructor(public element: HTMLDivElement) {
     element.addEventListener('click', this.clickEvent.bind(this))
   }
+
   clickEvent() {
     this.element.classList.toggle('count--active')
+    const score = Score.getInstance()
+    score.render()
   }
 }
 
 class Counts {
+  private static instance: Counts
   elements = document.querySelectorAll<HTMLDivElement>('.count')
-  constructor() {
+  private _activeElements: HTMLDivElement[] = []
+  private _activeElementsScore: number[] = []
+
+  get activeElements() {
+    this._activeElements = []
+    this.elements.forEach((e) => {
+      if (e.classList.contains('count--active')) {
+        this._activeElements.push(e)
+      }
+    })
+    return this._activeElements
+  }
+
+  get activeElementsScore() {
+    this._activeElementsScore = []
+    this.activeElements.forEach((e) => {
+      const score = e.querySelector('.count__score')
+      if (score) {
+        this._activeElementsScore.push(Number(score.textContent))
+      }
+    })
+    return this._activeElementsScore
+  }
+
+  private constructor() {
     this.elements.forEach((e) => {
       new Count(e)
     })
   }
+  static getInstance() {
+    if (!Counts.instance) {
+      Counts.instance = new Counts()
+    }
+    return Counts.instance
+  }
 }
 
-const counts = new Counts()
+const counts = Counts.getInstance()
